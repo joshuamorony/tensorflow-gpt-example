f46cee5059174d903e27bf52916684ce77c1c849
Joshua Morony <joshua.morony@gmail.com>
2023-04-14 21:29:41 +0930

    reshaped

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import * as tf from '@tensorflow/tfjs-node';
-import { Tensor1D } from '@tensorflow/tfjs-node';
+import {
+  Tensor1D,
+  Tensor2D,
+  Tensor3D,
+  TensorLike,
+} from '@tensorflow/tfjs-node';
+import { log } from 'console';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3333;
@@ -32,9 +38,6 @@ app.listen(port, host, () => {
     const sorted = Array.from(set).sort((a, b) => (a < b ? -1 : 1));
     const vocabSize = sorted.length;
 
-    console.log(sorted);
-    console.log(vocabSize);
-
     // map characters to integers
     const stoi = new Map<string, number>();
     // map integers to characters
@@ -54,11 +57,6 @@ app.listen(port, host, () => {
     }
 
     const data = tf.tensor1d(encode(text), 'int32');
-    // console.log('TENSOR1D');
-    // console.log(data.dataSync());
-    // console.log(data.shape, data.dtype);
-    // console.log('end print');
-    // console.log(data.slice(0, 1000).print());
 
     const trainLength = Math.floor(0.9 * data.size);
     const trainData = data.slice(0, trainLength);
@@ -66,15 +64,6 @@ app.listen(port, host, () => {
 
     const blockSize = 8;
     const batchSize = 4;
-
-    // const testX = trainData.slice(0, blockSize);
-    // const testY = trainData.slice(1, blockSize + 1);
-
-    // for (let i = 0; i < blockSize; i++) {
-    // const context = testX.slice(0, i + 1).dataSync();
-    // const target = testY.slice(i, 1).dataSync();
-    // console.log(`When input is ${context} the target is ${target}`);
-    // }
 
     const getBatch = (split: string) => {
       const batchData = split === 'train' ? trainData : validationData;
@@ -104,13 +93,13 @@ app.listen(port, host, () => {
     };
 
     const { contexts, targets } = getBatch('train');
-    console.log('inputs:');
-    console.log(contexts.shape);
-    console.log(contexts.arraySync());
-    console.log('targets:');
-    console.log(targets.shape);
-    console.log(targets.arraySync());
-    console.log('---');
+    // console.log('inputs:');
+    // console.log(contexts.shape);
+    // console.log(contexts.arraySync());
+    // console.log('targets:');
+    // console.log(targets.shape);
+    // console.log(targets.arraySync());
+    // console.log('---');
 
     for (let i = 0; i < batchSize; i++) {
       for (let j = 0; j < blockSize; j++) {
@@ -123,5 +112,105 @@ app.listen(port, host, () => {
         console.log(`When input is ${context} the target is ${target}`);
       }
     }
+
+    const model = new BigramLanguageModel(vocabSize);
+    const [logits, loss] = model.forward(contexts, targets);
+
+    // console.log(decode(model.generate(tf.zeros([1, 1]), 100)[0]));
+    // const idx = tf.zeros([1, 1], 'int32');
+    // const generated = model.generate(idx, 100).arraySync();
+    // console.log(decode(generated[0]));
   });
 });
+
+class BigramLanguageModel {
+  tokenEmbeddingTable: any;
+  constructor(private vocabSize: number) {
+    this.tokenEmbeddingTable = tf.layers.embedding({
+      inputDim: vocabSize,
+      outputDim: vocabSize,
+    });
+  }
+
+  forward(idx, targets = null): [Tensor3D, number] {
+    const logits = this.tokenEmbeddingTable.apply(idx);
+
+    let loss = null;
+
+    if (targets !== null) {
+      const [T, B, C] = logits.shape;
+      const logitsReshaped = logits.reshape([B * T, C]);
+      const targetsReshaped = targets.reshape([B * T]);
+
+      const oneHotLabels = tf.oneHot(
+        targetsReshaped,
+        logitsReshaped.shape[logitsReshaped.rank - 1]
+      );
+
+      loss = tf.losses.softmaxCrossEntropy(oneHotLabels, logitsReshaped);
+
+      // const oneHotLabels = tf.oneHot(targets, logits.shape[logits.rank - 1]);
+      // loss = tf.losses.softmaxCrossEntropy(oneHotLabels, logits);
+    }
+
+    return [logits, loss];
+  }
+
+  generate(idx, maxNewTokens) {
+    // for (let i = 0; i < maxNewTokens; i++) {
+    // // Get the predictions
+    // const [logits, loss] = this.forward(idx);
+    // console.log('logits');
+    // console.log(logits.print());
+    // // Split the logits tensor along the time axis
+    // const timeAxis = 1;
+    // const logitsSlices = tf.split(logits, logits.shape[timeAxis], timeAxis);
+    // console.log('slices');
+    // console.log(logitsSlices);
+    // // Focus only on the last time step
+    // const logitsLastStep = logitsSlices[logitsSlices.length - 1].reshape([
+    // logits.shape[0],
+    // logits.shape[2],
+    // ]);
+    // // Apply softmax to get probabilities
+    // const probs = logitsLastStep.softmax(-1);
+    // console.log('probs');
+    // console.log(probs.print());
+    // // Sample from the distribution
+    // // const idxNext = tf.multinomial(probs, 1);
+    // // console.log('next');
+    // // console.log(idxNext);
+    // // // Append sampled index to the running sequence
+    // // idx = tf.concat([idx, idxNext], 1);
+    // // console.log('new');
+    // // console.log(idx);
+    // }
+    // return idx;
+    // console.log('IDX');
+    // console.log(idx.print());
+    // for (let i = 0; i < maxNewTokens; i++) {
+    // // get predictions
+    // let [logits, loss] = this.forward(idx);
+    // console.log('preslice');
+    // console.log(logits.print());
+    // console.log(loss);
+    // // focus on last time step
+    // // TODO: check this - think this is incorrect for my setup
+    // logits = logits.slice([0, logits.shape[1] - 1, 0], [-1, 1, -1]); // becomes (B, C)
+    // console.log('sliced');
+    // console.log(logits.print());
+    // // apply softmax to get probabilities
+    // const probs = tf.softmax(logits, -1);
+    // console.log('probs');
+    // console.log(probs.print());
+    // // sample from distribution
+    // const idx_next = tf.multinomial(probs, 1);
+    // console.log('multinomial');
+    // console.log(idx_next.print());
+    // idx = tf.concat([idx, idx_next], 1); // (B, T+1)
+    // console.log('new');
+    // console.log(idx);
+    // }
+    // return idx;
+  }
+}
